132830a0015efe983e7e0f2a737a51d6b08c3d88
felunka <felixvontiedemann@gmail.com>
2024-06-16 11:33:57 +0200

    Add registration save

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,26 @@
+class RegistrationsController < ApplicationController
+  def create
+    @registration = Registration.new permit(params)
+
+    respond_to do |format|
+      if @registration.save
+        flash[:success] = t('messages.registration.created')
+        format.html { redirect_to root_path }
+      else
+        format.html { redirect_to root_path, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def permit(params)
+    params.require(:registration).permit(
+      registration_entries_attributes: %i[
+        name
+        food_selection
+        comment
+        all_days
+        _destroy
+      ]
+    )
+  end
+end
