f0547b7cca48ad82f496ec9124e46d7629b4c216
felunka <felixvontiedemann@gmail.com>
2024-11-03 22:29:24 +0100

    Put hotel rsvp fields inside collapse

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,20 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "@popperjs/core"
 import "bootstrap"
 import "cocoon-js-vanilla"
+import BootstrapTooltips from "./components/bootstrap_tooltips"
+
+// Fix popperjs
+window.process = {};
+window.process.env = {};
+window.process.env.NODE_ENV = "production";
+
+document.addEventListener("turbo:load", function() {
+  BootstrapTooltips.setup();
+  const rsvpButton = document.getElementById("hotel-rsvp-button")
+  rsvpButton.addEventListener("click", (_) => {
+    rsvpButton.classList.add("d-none")
+  });
+});
