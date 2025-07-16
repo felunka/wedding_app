3beec38cf7bf53a96061ea1ef10955fabd548db6
felunka <felixvontiedemann@gmail.com>
2025-07-16 23:01:20 +0200

    Reduce render quality of picture feed pictures and lazy load

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,8 @@
 class Picture < ApplicationRecord
   has_one_attached :data do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
-    attachable.variant :feed_sm, resize_to_limit: [450, 450], saver: { quality: 100 }
-    attachable.variant :feed_lg, resize_to_limit: [1200, 450], saver: { quality: 100 }
+    attachable.variant :feed_sm, resize_to_limit: [450, 450], saver: { quality: 80 }
+    attachable.variant :feed_lg, resize_to_limit: [1200, 450], saver: { quality: 80 }
   end
 
   scope :sorted, -> { order(:sort_index) }
